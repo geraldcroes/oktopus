85dc50fe4503b8ef5124a05fd7323d3aa1dafeca
Croës <gerald@croes.org>
2011-01-27 10:39:31 +0100

    Moving the debug files from the engine (they are now located in the debug directory)

---
+++ Documentation/index.php
@@ -0,0 +1,4 @@
+<h1>What is Oktopus ?</h1>
+<p>Oktopus is a PHP5.3 application engine that ease your everyday developpement, whatever technology or framework you're using.</p>
+
+<p>At the moment, Oktopus is just an Autoload System that you can add in your PHP project, simply using</p>
--- sandbox.php
+++ sandbox.php
@@ -3,6 +3,4 @@ require_once ('./Oktopus/Engine.class.php');
 Oktopus\Engine::start ('/tmp/Oktopus/');
 Oktopus\Autoloader::instance ()->addPath ('./Documentation/');
 
-$test = $test +1;
-
 echo "tout va bien";
