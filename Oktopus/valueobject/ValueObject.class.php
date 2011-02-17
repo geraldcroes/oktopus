90ac2d3592615fb43e1e27e6346abe9b93d4fa73
Croës <gerald@croes.org>
2011-02-17 22:11:33 +0100

    UnitTesting & changed the implementation with a private array of properties. Value objects no more compatible with get_object_vars but works better with isset

--- Oktopus/valueobject/ValueObject.class.php
+++ Oktopus/valueobject/ValueObject.class.php
@@ -13,162 +13,166 @@ namespace Oktopus;
  * @package Oktopus
  */
 class ValueObject implements \ArrayAccess {
-    /**
-     * Gets the property by its name
-     *
-     * @param mixed $pName
-     * @return mixed
-     */
-    public function &__get ($pName) {
-        return $this->$pName;
-    }
+	private $_properties = array();
+	
+	public function __isset ($pName) {
+		if (array_key_exists($pName, $this->_properties)){
+			if ($this->_properties[$pName] instanceof ValueObject){
+				$toInvoke = $this->_properties[$pName];
+				return $toInvoke(); 				
+			}
+			return isset($this->_properties[$pName]);
+		}
+		return false;
+	}
 
-    public function __isset ($pName) {
-        if ($this->$pName instanceof ValueObject) {
-            $test = get_object_vars($this->$pName);
-            return !empty($test);
-        }
-        return isset($this->$pName);
-    }
+	/**
+	 * Construct
+	 *
+	 * @param array $pArInit
+	 */
+	public function __construct ($pArInit = array ()) {
+		if (is_string($pArInit)) {
+			$exploded = explode(';', $pArInit);
+			$array = array();
 
-    /**
-     * Construct
-     *
-     * @param array $pArInit
-     */
-    public function __construct ($pArInit = array ()) {
-        if (is_string($pArInit)) {
-            $exploded = explode(';', $pArInit);
-            $array = array();
+			foreach ($exploded as $item) {
+				$item = explode('=>', $item);
+				if (count($item) == 2){
+					$array[$item[0]] = $item[1];
+				}else{
+					$array[] = $item[0];
+				}
+			}
+			$pArInit = $array;
+		}
 
-            foreach ($exploded as $item) {
-                $item = explode ('=>', $item);
-                if (count ($item) == 2){
-                    $array[$item[0]] = $item[1];
-                }else{
-                    $array[] = $item[0];
-                }
-            }
-            $pArInit = $array;
-        }
+		if (is_array($pArInit)) {
+			foreach ($pArInit as $key => $item) {
+				$this->$key = $item;
+			}
+		} elseif (is_object($pArInit)) {
+			if ($pArInit instanceof ValueObject){
+				foreach ($pArInit->asArray() as $key => $item) {
+					$this->$key = $item;
+				}				
+			}else{
+				foreach (get_object_vars($pArInit) as $key => $item) {
+					$this->$key = $item;
+				}
+			}
+		}
+	}
 
-        if (is_array ($pArInit)) {
-            foreach ($pArInit as $key => $item) {
-                $this->$key = $item;
-            }
-        } elseif (is_object ($pArInit)) {
-            foreach (get_object_vars ($pArInit) as $key => $item) {
-                $this->$key = $item;
-            }
-        }
-    }
+	public function offsetGet ($pOffset) {
+		return $this->$pOffset;
+	}
 
-    public function offsetGet ($pOffset) {
-        return $this->$pOffset;
-    }
+	public function offsetSet ($pOffset, $pValue) {
+		if ($pOffset === null) {
+			if (count($this->_properties) === 0) {
+				$pOffset = 0;
+			} else {
+				$pOffset = max(array_keys($this->_properties)) + 1;
+			}
+		}
+		$this->$pOffset = $pValue;
+	}
 
-    public function offsetSet ($pOffset, $pValue) {
-        if ($pOffset === null) {
-            $vars = get_object_vars($this);
-            if (count($vars) === 0) {
-                $pOffset = 0;
-            } else {
-                $pOffset = max(array_keys(get_object_vars ($this))) + 1;
-            }
-        }
-        $this->$pOffset = $pValue;
-    }
+	public function offsetExists ($pOffset) {
+		return $this->__isset($pOffset);
+	}
 
-    public function offsetExists ($pOffset) {
-        return isset($this->$pOffset);
-    }
+	public function offsetUnset ($pOffset) {
+		$this->$pOffset = new ValueObject();
+	}
 
-    public function offsetUnset ($pOffset) {
-        $this->$pOffset = null;
-    }
+	public function mergeWith ($pToMerge) {
+		if (is_object($pToMerge)) {
+			if ($pToMerge instanceof ValueObject) {
+				$pToMerge = $pToMerge->asArray();
+			} else {
+				$pToMerge = get_object_vars($pToMerge);
+			}
+		} elseif (!is_array($pToMerge)) {
+			$pToMerge = array($pToMerge);
+		}
 
-    public function merge ($pToMerge) {
-        if (is_array ($pToMerge)) {
-            $pToMerge = new ValueObject($pToMerge);
-        } else if (is_object ($pToMerge)) {
-            if (!($pToMerge instanceof ValueObject)) {
-                $pToMerge = new ValueObject(get_object_vars($pToMerge));
-            }
-        } else {
-            $pToMerge = array ($pToMerge);
-        }
+		foreach ($pToMerge as $name => $prop) {
+			if (!isset($this->$name)){
+				$this->$name = $prop;
+			}
+		}
+		return $this;
+	}
 
-        foreach (get_object_vars ($pToMerge) as $name => $prop) {
-            if (!isset ($this->$name)){
-                $this->$name = $prop;
-            }
-        }
-        return $this;
-    }
+	public function loadFrom ($pData, $pCreateNew = true){
+		$valueObject = new ValueObject($pData);
+		$valueObject->saveIn($this, $pCreateNew);
+		return $this;
+	}
 
-    public function loadFrom ($pData, $pCreateNew = true){
-        $valueObject = new ValueObject($pData);
-        $valueObject->saveIn($this, $pCreateNew);
-        return $this;
-    }
+	public function saveIn (&$pDest, $pCreateNew = true) {
+		//détermine le "type" de l'objet
+		if (!($array = is_array ($pDest))) {
+			if (!($object = is_object ($pDest))) {
+				$natural = true;
+			}
+		}
+		$elementVars = array ();
+		if ($array || $object) {
+			$elementVars = array_keys ($this->_getElementVars ($pDest));
+		}
 
-    public function saveIn (&$pDest, $pCreateNew = true) {
-        //détermine le "type" de l'objet
-        if (!($array = is_array ($pDest))) {
-            if (!($object = is_object ($pDest))) {
-                $natural = true;
-            }
-        }
-        $elementVars = array ();
-        if ($array || $object) {
-            $elementVars = array_keys ($this->_getElementVars ($pDest));
-        }
+		//on parcours chacune des propriétés de l'élément
+		foreach (get_object_vars ($this) as $name => $element) {
+			//on regarde si la propriété existe dans la destination
+			if (($inArray = in_array ($name, $elementVars)) || $pCreateNew) {
+				if ($inArray && (is_object ($element) || is_array ($element))) {
+					//la propriété existait déja et c'est un tableau ou un objet,
+					//on reparcours le tout pour y appliquer les changements
+					if ($array) {
+						$valueObject = new ValueObject($element);
+						$valueObject->saveIn ($pDest[$name], $pCreateNew);
+					} else {
+						$valueObject = new ValueObject($element);
+						$valueObject->saveIn ($pDest->$name, $pCreateNew);
+					}
+					// NOTE : il n'est pas possible d'avoir recours a l'opérateur
+					// ternaire pour les passages par référence
+				} else {
+					// la propriété n'existait pas => il faut la créer a l'identique
+					// ou la propriété existait sous sa forme naturelle et il faut la remplacer
+					if ($array) {
+						$pDest[$name] = $element;
+					} else {
+						$pDest->$name = $element;
+					}
+				}
+			}
+		}
 
-        //on parcours chacune des propriétés de l'élément
-        foreach (get_object_vars ($this) as $name => $element) {
-            //on regarde si la propriété existe dans la destination
-            if (($inArray = in_array ($name, $elementVars)) || $pCreateNew) {
-                if ($inArray && (is_object ($element) || is_array ($element))) {
-                    //la propriété existait déja et c'est un tableau ou un objet,
-                    //on reparcours le tout pour y appliquer les changements
-                    if ($array) {
-                        $valueObject = new ValueObject($element);
-                        $valueObject->saveIn ($pDest[$name], $pCreateNew);
-                    } else {
-                        $valueObject = new ValueObject($element);
-                        $valueObject->saveIn ($pDest->$name, $pCreateNew);
-                    }
-                    // NOTE : il n'est pas possible d'avoir recours a l'opérateur
-                    // ternaire pour les passages par référence
-                } else {
-                    // la propriété n'existait pas => il faut la créer a l'identique
-                    // ou la propriété existait sous sa forme naturelle et il faut la remplacer
-                    if ($array) {
-                        $pDest[$name] = $element;
-                    } else {
-                        $pDest->$name = $element;
-                    }
-                }
-            }
-        }
+		return $this;
+	}
 
-        return $this;
-    }
+	protected function _getElementVars ($pElement) {
+		return is_array ($pElement) ? $pElement : ($pElement instanceof ValueObject ? $pElment->asArray() : get_object_vars($pElement));
+	}
 
-    protected function _getElementVars ($pElement) {
-        return (is_array ($pElement)) ? $pElement : get_object_vars ($pElement);
-    }
-}
+	public function __invoke() {
+		return count($this->_properties) === 0 ? null : $this->__toString();
+	}
 
-/**
- * RecursiveValueObject
- *
-
- * @package Oktopus
- */
-class RecursiveValueObject extends ValueObject {
 	public function & __get ($pName) {
-		$this->$pName = new RecursiveValueObject ();
-		return $this->$pName;
+		if (! array_key_exists($pName, $this->_properties)){
+			$this->_properties[$pName] = new ValueObject();
+		}
+		return $this->_properties[$pName];
+	}
+	public function __set($pName, $pValue){
+		$this->_properties[$pName] = $pValue;
+	}
+	public function asArray () {
+		return $this->_properties;
 	}
 }
--- tests/valueobject/ValueObjectTest.php
+++ tests/valueobject/ValueObjectTest.php
@@ -1,53 +1,119 @@
 <?php
 class ValueObjectTest extends PHPUnit_Framework_TestCase {
-   public function setUp () {
-      require_once __DIR__.'/../bootstrap.php';
-   }
+	public function setUp () {
+		require_once __DIR__.'/../bootstrap.php';
+	}
 
 
-    public function testCreate (){
-        //Creating a value object from an array
-        $test = array('p1'=>1, 'p2'=>2, 'p3'=>3);
-        $valueObject = new Oktopus\ValueObject($test);
-        
-        $this->assertEquals($valueObject->p1, $test['p1']);
-        $this->assertEquals($valueObject->p2, $test['p2']);
-        $this->assertEquals($valueObject->p3, $test['p3']);
-        
-        $valueObject = new Oktopus\ValueObject('p1=>1;p2=>2;p3=>3;test;test2');
-        $this->assertEquals($valueObject->p1, '1');
-        $this->assertEquals($valueObject->p2, '2');
-        $this->assertEquals($valueObject->p3, '3');
-	$this->assertEquals($valueObject[0], 'test');
-	$this->assertEquals($valueObject[1], 'test2');
+	public function testCreate (){
+		//Creating a value object from an array
+		$test = array('p1'=>1, 'p2'=>2, 'p3'=>3);
+		$valueObject = new Oktopus\ValueObject($test);
+		$this->assertEquals($valueObject->p1, $test['p1']);
+		$this->assertEquals($valueObject->p2, $test['p2']);
+		$this->assertEquals($valueObject->p3, $test['p3']);
 
-	$stdC = new StdClass();
-	$stdC->test  = 1;
-	$stdC->test2 = 2;
-	$stdC->test3 = 3;
-	$valueObject = new Oktopus\ValueObject($stdC);
-	$this->assertEquals($valueObject->test, 1);
-	$this->assertEquals($valueObject->test2, 2);
-	$this->assertEquals($valueObject->test3, 3);
-    }
+		$valueObject = new Oktopus\ValueObject('p1=>1;p2=>2;p3=>3;test;test2');
+		$this->assertEquals($valueObject->p1, '1');
+		$this->assertEquals($valueObject->p2, '2');
+		$this->assertEquals($valueObject->p3, '3');
+		$this->assertEquals($valueObject[0], 'test');
+		$this->assertEquals($valueObject[1], 'test2');
 
-   public function testIsset () {
-      $test = 'p1=>1;p2=>2;p3=>3';
-      $valueObject = new Oktopus\ValueObject($test);
-      
-      $this->assertFalse(isset($valueObject->notSet));
-      $this->assertTrue (isset($valueObject->p1));
+		$stdC = new StdClass();
+		$stdC->test  = 1;
+		$stdC->test2 = 2;
+		$stdC->test3 = 3;
+		$valueObject = new Oktopus\ValueObject($stdC);
+		$this->assertEquals($valueObject->test, 1);
+		$this->assertEquals($valueObject->test2, 2);
+		$this->assertEquals($valueObject->test3, 3);
+		
+		$valueObject = new Oktopus\ValueObject($valueObject);
+		$this->assertEquals($valueObject->test, 1);
+		$this->assertEquals($valueObject->test2, 2);
+		$this->assertEquals($valueObject->test3, 3);
+	}
 
-      $this->assertFalse(isset($valueObject['notSet']));
-      $this->assertTrue(isset($valueObject['p1']));
-   }
+	public function testIsset () {
+		$test = 'p1=>1;p2=>2;p3=>3';
+		$valueObject = new Oktopus\ValueObject($test);
 
-   public function testUnset () {
-      $valueObject = new Oktopus\ValueObject('p1=>1;p2=>2;p3=>3');
-      $this->assertTrue(isset($valueObject['p1']));
-      unset($valueObject['p1']);
+		$this->assertFalse(isset($valueObject->notSet));
+		$this->assertTrue (isset($valueObject->p1));
 
-      $this->assertFalse(isset($valueObject['p1']));
-      $this->assertFalse(isset($valueObject->p1));
-   }
+		$this->assertFalse(isset($valueObject['notSet']));
+		$this->assertTrue(isset($valueObject['p1']));
+	}
+
+	public function testUnset () {
+		$valueObject = new Oktopus\ValueObject('p1=>1;p2=>2;p3=>3');
+		$this->assertTrue(isset($valueObject['p1']));
+		unset($valueObject['p1']);
+
+		$this->assertFalse(isset($valueObject['p1']));
+		$this->assertFalse(isset($valueObject->p1));
+	}
+	
+	public function testArray() {
+		$valueObject = new Oktopus\ValueObject('p1=>1;p2=>2;p3=>3;4');
+		$valueObject[] = '5';
+		$this->assertEquals($valueObject[1], '5');
+		$valueObject[1] = '6';
+		$this->assertEquals($valueObject[1], '6');
+		$valueObject[1] = '7';
+		$this->assertEquals($valueObject[1], '7');
+
+		$valueObject = new Oktopus\ValueObject();
+		$valueObject[] = '5';
+		$this->assertEquals($valueObject[0], '5');
+		$valueObject[1] = '6';
+		$this->assertEquals($valueObject[1], '6');
+		$valueObject[1] = '7';
+		$this->assertEquals($valueObject[1], '7');
+		
+		$valueObject[2][] = '7';
+		$this->assertEquals($valueObject[2][0], '7');
+	}
+	
+	public function testMerge () {
+		$test = array('p1'=>1, 'p2'=>2, 'p3'=>3);
+		$valueObject = new Oktopus\ValueObject($test);
+		$valueObject2 = new Oktopus\ValueObject(array('p4'=>4));
+		$valueObject->mergeWith($valueObject2);
+		$this->assertEquals($valueObject->p1, $test['p1']);
+		$this->assertEquals($valueObject->p2, $test['p2']);
+		$this->assertEquals($valueObject->p3, $test['p3']);
+		$this->assertEquals($valueObject->p4, 4);
+		$this->assertFalse(isset($valueObject2->p1));
+		$this->assertFalse(isset($valueObject2->p2));		
+		$this->assertFalse(isset($valueObject2->p3));		
+		
+		$valueObject = new Oktopus\ValueObject($test);
+		$valueObject2 = new Oktopus\ValueObject(array('p4'=>4));
+		$valueObject2->mergeWith($valueObject);
+		$this->assertEquals($valueObject2->p1, $test['p1']);
+		$this->assertEquals($valueObject2->p2, $test['p2']);
+		$this->assertEquals($valueObject2->p3, $test['p3']);
+		$this->assertEquals($valueObject2->p4, 4);
+		$this->assertFalse(isset($valueObject->p4));		
+		
+		$valueObject2 = new Oktopus\ValueObject(array('p4'=>4));
+		$valueObject2->mergeWith($test);
+		$this->assertEquals($valueObject2->p1, $test['p1']);
+		$this->assertEquals($valueObject2->p2, $test['p2']);
+		$this->assertEquals($valueObject2->p3, $test['p3']);
+		$this->assertEquals($valueObject2->p4, 4);
+
+		$test = new StdClass();
+		$test->p1 = 1;
+		$test->p2 = 2;
+		$test->p3 = 3;
+		$valueObject2 = new Oktopus\ValueObject(array('p4'=>4));
+		$valueObject2->mergeWith($test);
+		$this->assertEquals($valueObject2->p1, $test->p1);
+		$this->assertEquals($valueObject2->p2, $test->p2);
+		$this->assertEquals($valueObject2->p3, $test->p3);
+		$this->assertEquals($valueObject2->p4, 4);
+	}
 }
