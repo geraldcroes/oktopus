b13078a7ab018f3de624e15667049b09e4fbebee
Croës <gerald@croes.org>
2011-07-25 18:49:45 +0200

    Bootstrap declares the production mode as a default for the Oktopus Engine

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/mageekguy.atoum.phar';
 require (__DIR__.'/../Oktopus/Engine.php');
-Oktopus\Engine::start ('/tmp/');
+Oktopus\Engine::start ('/tmp/', Oktopus\Engine::MODE_PRODUCTION);
 Oktopus\Debug::unregisterErrorHandler();
 Oktopus\Debug::unregisterExceptionHandler();
