6bdcb8f4d1296c43e32e93c04f0523f70a7f1e8b
Croës <gerald@croes.org>
2011-07-25 18:49:16 +0200

    [WIP] Added more Tests with Atoum

--- tests/engine/Autoloader.php
+++ tests/engine/Autoloader.php
@@ -37,5 +37,82 @@ class Autoloader extends atoum\test {
         $this->assert
                 ->boolean($autoloader->autoload('foo'))
                 ->isFalse();
-	}	
+	}
+
+    /**
+     * Tries to autoload a previously known class (in the cache) that has been moved around in production mode
+     */
+	public function testAutoloadKnownClassThatHasBeenMovedProductionMode (){
+		//Remove old cache file if it exists and copy sources to test
+		exec('rm -Rf /tmp/OktopusTest/testDeleteFile/tmp/');
+		exec('rm -Rf /tmp/OktopusTest/testDeleteFile/sources/');
+		exec('cp -R '.__DIR__.'/../resources/nowarning /tmp/OktopusTest/testDeleteFile/sources/');
+
+		//will generate cache file for every classes (including foo)
+        $autoloader = new \Oktopus\Autoloader('/tmp/OktopusTest/testDeleteFile/tmp/', new ClassParserForPHP5_3());
+		$autoloader->addPath('/tmp/OktopusTest/testDeleteFile/sources/');
+		$this->assert
+                ->boolean($autoloader->autoload ('foo2'))
+                ->isTrue();
+
+		$autoloader = new \Oktopus\Autoloader('/tmp/OktopusTest/testDeleteFile/tmp/', new ClassParserForPHP5_3());
+		$autoloader->addPath('/tmp/OktopusTest/testDeleteFile/sources/');
+
+		exec('mv /tmp/OktopusTest/testDeleteFile/sources/foo.php /tmp/OktopusTest/testDeleteFile/sources/foomoved.php');
+
+		//This test should be run considering Oktopus is in PRODUCTION_MODE
+        $this->assert
+                ->boolean(\Oktopus\Engine::getMode() === \Oktopus\Engine::MODE_PRODUCTION)
+                ->isTrue();
+
+		//Tries to autoload the moved class without allowing the autoloader to refresh its cache
+        $this->assert
+                ->boolean($autoloader->autoload('foo', false))
+                ->isFalse();
+
+        //Tries to autoload the moved class allowing the autoloader to refresh its cache
+        $this->assert
+                ->boolean($autoloader->autoload('foo', true))
+                ->isTrue();
+	}
+
+    /**
+     * Tries to autoload a previously known class (in the cache) that has been moved around not in production mode
+     */
+	public function testAutoloadKnownClassThatHasBeenMovedNotProductionMode (){
+        //Restarting the engine in debug mode
+        \Oktopus\Engine::start ('/tmp/', \Oktopus\Engine::MODE_DEBUG);
+
+		//Remove old cache file if it exists and copy sources to test
+		exec('rm -Rf /tmp/OktopusTest/testDeleteFile/tmp/');
+		exec('rm -Rf /tmp/OktopusTest/testDeleteFile/sources/');
+		exec('cp -R '.__DIR__.'/../resources/nowarning /tmp/OktopusTest/testDeleteFile/sources/');
+
+		//will generate cache file for every classes (including foo)
+        $autoloader = new \Oktopus\Autoloader('/tmp/OktopusTest/testDeleteFile/tmp/', new ClassParserForPHP5_3());
+		$autoloader->addPath('/tmp/OktopusTest/testDeleteFile/sources/');
+		$this->assert
+                ->boolean($autoloader->autoload ('foo2'))
+                ->isTrue();
+
+		$autoloader = new \Oktopus\Autoloader('/tmp/OktopusTest/testDeleteFile/tmp/', new ClassParserForPHP5_3());
+		$autoloader->addPath('/tmp/OktopusTest/testDeleteFile/sources/');
+
+		exec('mv /tmp/OktopusTest/testDeleteFile/sources/foo.php /tmp/OktopusTest/testDeleteFile/sources/foomoved.php');
+
+		//This test should be run considering Oktopus is in PRODUCTION_MODE
+        $this->assert
+                ->boolean(\Oktopus\Engine::getMode() !== \Oktopus\Engine::MODE_PRODUCTION)
+                ->isTrue();
+
+		//Tries to autoload the moved class without allowing the autoloader to refresh its cache
+        $this->assert
+                ->boolean($autoloader->autoload('foo', false))
+                ->isTrue();
+
+        //Tries to autoload the moved class allowing the autoloader to refresh its cache
+        $this->assert
+                ->boolean($autoloader->autoload('foo', true))
+                ->isTrue();
+	}
 }
