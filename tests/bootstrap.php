848161d54ada89d9c7729f55e173e2ad868678a1
Croës <gerald@croes.org>
2011-07-29 16:29:40 +0200

    require => require_once

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
-require __DIR__ . '/mageekguy.atoum.phar';
-require (__DIR__.'/../Oktopus/Engine.php');
+require_once __DIR__ . '/mageekguy.atoum.phar';
+require_once (__DIR__.'/../Oktopus/Engine.php');
 Oktopus\Engine::start ('/tmp/', Oktopus\Engine::MODE_PRODUCTION);
 Oktopus\Debug::unregisterErrorHandler();
 Oktopus\Debug::unregisterExceptionHandler();
