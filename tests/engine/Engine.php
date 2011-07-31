d999ef520b3ad8b91f0c0900ea2106b92dfeec8e
Croës <gerald@croes.org>
2011-07-31 11:55:44 +0200

    moved engine's test into Engine test class

--- tests/engine/Engine.php
+++ tests/engine/Engine.php
@@ -33,5 +33,28 @@ class Engine extends atoum\test {
         $this->assert
                 ->string(ini_get('display_errors'))
                 ->isEqualTo('0');
+        $this->assert->variable(\Oktopus\Engine::getMode())
+                     ->isEqualTo(\Oktopus\Engine::MODE_PRODUCTION);
 	}
+
+    public function testGetTemporaryFilePath()
+    {
+        $this->assert->variable(\Oktopus\Engine::getTemporaryFilesPath())->isNull();
+    }
+
+    public function testGetAutoloader ()
+    {
+        //Asserting that engine's autoloader cannot be accessed until engine's start up
+        $this->assert
+                ->exception(function(){\Oktopus\Engine::autoloader();})
+                ->isInstanceOf('Exception');
+
+        //Once started, assert that the autoloader can be accessed and has the right temporary path
+        \Oktopus\Engine::start('/tmp/');
+        $this->assert
+                ->object(\Oktopus\Engine::autoloader())
+                ->isInstanceOf('\Oktopus\Autoloader')
+                ->string(\Oktopus\Engine::autoloader()->getCachePath())
+                ->isEqualTo(\Oktopus\Engine::getTemporaryFilesPath());
+    }
 }
